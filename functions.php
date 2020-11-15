aaca92e8138285b00682702cf385b936743f1f7b
Sam Connolly <sconnolly5@uclan.ac.uk>
2020-11-15 22:54:50 +0000

    Added multiple textboxes and customisation options to advanced product tab

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+
+define("MAX_NUM_CUSTOM_TEXT_BOXES", 5);
+
 // enqueue the child theme stylesheet
 function the_little_trinket_box_child_theme_enqueue_scripts() {
     wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css' );
@@ -9,26 +12,85 @@ add_action( 'wp_enqueue_scripts', 'the_little_trinket_box_child_theme_enqueue_sc
 /**
  * Define the option to add a customisable text field under the 
  */
-function the_little_trinket_box_adv_product_options(){
- 
-	echo '<div class="options_group">';
- 
-	woocommerce_wp_checkbox( array(
-		'id'      => 'customisable_text',
-		'value'   => get_post_meta( get_the_ID(), 'customisable_text', true ),
-		'label'   => 'Enable customisable text',
-		'desc_tip' => true,
-		'description' => 'This product should show the user a text box to customise a product.',
-	) );
- 
-	echo '</div>';
- 
+function the_little_trinket_box_adv_product_options() {
+    $customTextBoxCount = defined('MAX_NUM_CUSTOM_TEXT_BOXES') ? constant('MAX_NUM_CUSTOM_TEXT_BOXES') : 0;
+
+    for ($i = 1; $i <= $customTextBoxCount; $i++) {
+        echo '<div class="options_group">';
+        woocommerce_wp_checkbox( array(
+            'id'      => 'custom_text_enabled_' . strval($i),
+            'value'   => get_post_meta( get_the_ID(), 'custom_text_enabled_' . strval($i), true ),
+            'label'   => 'Custom text box ' . strval($i) . ' enabled?',
+            'desc_tip' => true,
+            'description' => 'This checkbox should be enabled when there is a requirement to show custom text area ' . strval($i) . '.',
+        ));
+
+        woocommerce_wp_checkbox( array(
+            'id'      => 'custom_text_is_optional_' . strval($i),
+            'value'   => get_post_meta( get_the_ID(), 'custom_text_is_optional_' . strval($i), true ),
+            'label'   => 'Can be empty?',
+            'desc_tip' => true,
+            'description' => 'Custom text boxes can be left blank if this is checked',
+        ));
+
+        // Text Field
+        woocommerce_wp_text_input( 
+            array( 
+                'id'          => 'custom_text_field_label_' . strval($i), 
+                'value'       => get_post_meta( get_the_ID(), 'custom_text_field_label_' . strval($i), true ),
+                'label'       => __( 'Label to show next to text field', 'woocommerce' ), 
+                'placeholder' => 'Label to show',
+                'desc_tip'    => 'true',
+                'description' => __( 'Enter the value that should be shown next to custom text field number ' . strval($i), 'woocommerce' ) 
+            ));
+        
+        // Max length
+        $max_len_default_value = get_post_meta( get_the_ID(), 'custom_text_field_max_length_' . strval($i), true );
+        if (!isset($max_len_default_value) || $max_len_default_value == '') 
+        {
+            // Default to 200 characters
+            $max_len_default_value = 200;
+        }
+
+        woocommerce_wp_text_input( 
+            array( 
+                'id'                => 'custom_text_field_max_length_' . strval($i), 
+                'value'             => $max_len_default_value,
+                'label'             => "Max length", 
+                'placeholder'       => 'Enter the max num of characters', 
+                'desc_tip'          => true,
+                'description'       => "Define the maximum number of characters that can be input into this text box",
+                'type'              => 'number', 
+                'custom_attributes' => array(
+                        'step' 	=> 'any',
+                        'min'	=> '0'
+                    ) 
+            )
+        );
+
+        // Allow symbols?
+        woocommerce_wp_checkbox( array(
+            'id'      => 'custom_text_field_allow_symbols_' . strval($i),
+            'value'   => get_post_meta( get_the_ID(), 'custom_text_field_allow_symbols_' . strval($i), true ),
+            'label'   => 'Allow symbols?',
+            'desc_tip' => true,
+            'description' => 'Symbols should be accepted within the customisable text ' . strval($i),
+        ));
+        echo '</div>';
+    }    
 }
 add_action( 'woocommerce_product_options_advanced', 'the_little_trinket_box_adv_product_options');
  
  
-function the_little_trinket_box_save_fields( $id, $post ){
-    update_post_meta( $id, 'customisable_text', $_POST['customisable_text'] );
+function the_little_trinket_box_save_fields( $id, $post ) {
+    $customTextBoxCount = defined('MAX_NUM_CUSTOM_TEXT_BOXES') ? constant('MAX_NUM_CUSTOM_TEXT_BOXES') : 0;
+    for ($i = 1; $i <= $customTextBoxCount; $i++) {
+        update_post_meta( $id, 'custom_text_enabled_' . strval($i), $_POST['custom_text_enabled_' . strval($i)] );
+        update_post_meta( $id, 'custom_text_is_optional_' . strval($i), $_POST['custom_text_is_optional_' . strval($i)] );
+        update_post_meta( $id, 'custom_text_field_label_' . strval($i), $_POST['custom_text_field_label_' . strval($i)] );
+        update_post_meta( $id, 'custom_text_field_max_length_' . strval($i), $_POST['custom_text_field_max_length_' . strval($i)] );
+        update_post_meta( $id, 'custom_text_field_allow_symbols_' . strval($i), $_POST['custom_text_field_allow_symbols_' . strval($i)] );
+    }
 }
 add_action( 'woocommerce_process_product_meta', 'the_little_trinket_box_save_fields', 10, 2 );
 
@@ -38,12 +100,30 @@ add_action( 'woocommerce_process_product_meta', 'the_little_trinket_box_save_fie
 function the_little_trinket_box_custom_option(){
     global $product;
     $id = $product->get_id();
-    $customisable_product = get_post_meta( $id, 'customisable_text', true);
-    $value = isset( $_POST['_custom_option' ] ) ? sanitize_text_field( $_POST['_custom_option'] ) : '';
-    if ($customisable_product)
-    {
-        printf( '<table class="variations" cellspacing="0" style="margin-bottom: 10px;"><tbody><tr><td><label style="margin-right: 10px;" for="_custom_option">%s</label></td><td class="value"><input class="input-text text" name="_custom_option" value="%s"/></td></tr></tbody></table>', __( 'Custom text: ', 'the-little-trinket-box-plugin-textdomain' ), esc_attr( $value ) );    
+
+    $customTextBoxCount = defined('MAX_NUM_CUSTOM_TEXT_BOXES') ? constant('MAX_NUM_CUSTOM_TEXT_BOXES') : 0;
+
+    $values = [];
+    for ($i = 1; $i <= $customTextBoxCount; $i++) {
+        $customOption = '_custom_option_' . strval($i);
+        $valueToAdd = isset( $_POST[$customOption] ) ? sanitize_text_field( $_POST[$customOption] ) : '';
+        array_push($values, $valueToAdd);
     }
+
+    $customTextboxExists = false;
+    $tableHtml = '<table class="variations" cellspacing="0" style="margin-bottom: 10px;"><tbody>';
+    for ($i = 1; $i <= $customTextBoxCount; $i++) {
+        $customTextEnabled = get_post_meta($id, 'custom_text_enabled_' . strval($i), true) == "yes" ? true : false;
+        if ($customTextEnabled) {
+            $customTextboxExists = true;
+            $customLabel = get_post_meta($id, 'custom_text_field_label_' . strval($i), true);
+            $maxlen = get_post_meta($id, 'custom_text_field_max_length_' . strval($i), true);
+            $tableHtml .= '<tr><td><label style="margin-right: 10px;" for="_custom_option_' . strval($i) . '">' . $customLabel . '</label></td><td class="value"><input class="input-text text" name="_custom_option_' . strval($i).'" value="' . esc_attr( $values[$i] ) . '" maxlength="' . $maxlen .'"/></td></tr>';
+        }
+    }
+    $tableHtml .= '</tbody></table>';
+    printf($tableHtml);
+
 }
 add_action( 'woocommerce_before_add_to_cart_button', 'the_little_trinket_box_custom_option', 9);
 
