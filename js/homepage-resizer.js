9d848de5a3dc89285440aba68c6e3505942d2797
Sam Connolly <sconnolly5@uclan.ac.uk>
2021-01-18 20:14:28 +0000

    Refactored code to improve readability, removed call to remove overlay from sliders section.

--- js/homepage-resizer.js
+++ js/homepage-resizer.js
@@ -1,34 +1,13 @@
 jQuery(function ($) { 
     $(document).ready(function() {
-        var banner = document.querySelectorAll('.shop_isle_bannerss_section');
+        AdjustBannerTo4Elements();
+        ResizeLatestProductsContainers($);
+        // DisableGreyOverlay()
+    });
 
-        for (var i = 0; i < banner.length; i++) {
-            var bannerItems = banner[i].querySelectorAll(':scope > .col-sm-4');
-            for (var j = 0; j < bannerItems.length; j++) {
-                $(bannerItems[j]).removeClass('col-sm-4');
-                $(bannerItems[j]).addClass('col-sm-3');
-            }
-        }
-
-        //col-sm-5 col-md-2 col-lg-2
-        var latest = document.querySelector('#latest');
-        var latestContainer = latest.querySelector('.container');
-        var rows = latestContainer.querySelector(':scope > .multi-columns-row');
-        var divs = rows.querySelectorAll(':scope > div');
-        for (var i = 0; i < divs.length; i++) {
-            $(divs[i]).removeClass('col-sm-6');
-            $(divs[i]).addClass('col-sm-3');
-            
-            // $(divs[i]).removeClass('col-md-3');
-            // $(divs[i]).addClass('col-md-2');
-
-             $(divs[i]).removeClass('col-lg-3');
-             $(divs[i]).addClass('col-lg-2');
-        }
-
+    function DisableGreyOverlay() {
         // Remove overlay from slides
         var slides = document.querySelector('.slides').childNodes;
-        console.log(slides);
         for (var i = 0; i < slides.length; i++) {
             var ele = slides[i];
             ele = $(ele);
@@ -41,8 +20,37 @@ jQuery(function ($) {
             }
             ele.addClass('bg-light');
         }
-        console.log("-------------------");
-        console.log(slides);
-        // console.log(slides.querySelectorAll(':scope > .bg-dark'));
-    });
+    }
+
+    /**
+     * Currently the banner takes 3 elements to fill the width of the screen, this function will adjust the bootstrap classes applied to banner containers to take 4 to fill the width.
+     */
+    function AdjustBannerTo4Elements() {
+        var banner = document.querySelectorAll('.shop_isle_bannerss_section');
+        for (var i = 0; i < banner.length; i++) {
+            var bannerItems = banner[i].querySelectorAll(':scope > .col-sm-4');
+            for (var j = 0; j < bannerItems.length; j++) {
+                $(bannerItems[j]).removeClass('col-sm-4');
+                $(bannerItems[j]).addClass('col-sm-3');
+            }
+        }
+    }
+    
+    /**
+     * The latest products category can often look too large with resized banner, adjust the bootstrap classes here to make them smaller.
+     */
+    function ResizeLatestProductsContainers() {
+        var latest = document.querySelector('#latest');
+        var latestContainer = latest.querySelector('.container');
+        var rows = latestContainer.querySelector(':scope > .multi-columns-row');
+        var divs = rows.querySelectorAll(':scope > div');
+        for (var i = 0; i < divs.length; i++) {
+            $(divs[i]).removeClass('col-sm-6');
+            $(divs[i]).addClass('col-sm-3');
+            // $(divs[i]).removeClass('col-md-3');
+            // $(divs[i]).addClass('col-md-2');
+            $(divs[i]).removeClass('col-lg-3');
+            $(divs[i]).addClass('col-lg-2');
+        }
+    }
 });
